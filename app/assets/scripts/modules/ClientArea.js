1a804b0b8703f10c05f531c276de358af397e3ab
Jerry Jason <jerryjason7@live.com>
2020-08-20 12:39:45 -0500

    fix bottom

--- app/assets/scripts/modules/ClientArea.js
+++ app/assets/scripts/modules/ClientArea.js
@@ -34,16 +34,18 @@ class ClientArea {
 	injectHTML() {
 		document.body.insertAdjacentHTML(
 			'beforeend',
-			`<div class="client-area">
-        <div class="wrapper wrapper--medium">
-          <h2 class="section-title section-title--blue">Secret Client Area</h2>
-          <form class="client-area__form" action="">
-            <input class="client-area__input" type="text" placeholder="Enter the secret phrase">
-            <button class="btn btn--orange">Submit</button>
-          </form>
-          <div class="client-area__content-area"></div>
-        </div>
-      </div>`
+			`
+    <div class="client-area" style="margin-bottom: 50px;">
+      <div class="wrapper wrapper--medium">
+        <h2 class="section-title section-title--blue">Secret Client Area</h2>
+        <form class="client-area__form" action="">
+          <input class="client-area__input" type="text" placeholder="Enter the secret phrase">
+          <button class="btn btn--orange">Submit</button>
+        </form>
+        <div class="client-area__content-area"></div>
+      </div>
+    </div>
+    `
 		);
 	}
 }
